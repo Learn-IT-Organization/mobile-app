33aeb1d11e86efe9cc6faec6b4568ec52dc0ece1
KrisztaOniga <99740799+KrisztaOniga@users.noreply.github.com>
2024-01-09 22:21:05 +0200

    #62: Base Quiz Fragment connection by type

--- app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/MultipleChoiceQuizFragment.kt
+++ app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/MultipleChoiceQuizFragment.kt
@@ -1,42 +1,29 @@
-package com.example.learnit.ui.feature.courses.quiz.fragment
+package com.example.learnit.ui.feature.cou
 
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.example.learnit.data.courses.quiz.mapper.mapToAnswerData
-import com.example.learnit.data.courses.quiz.model.AnswerData
-import com.example.learnit.data.courses.quiz.model.UserAnswerData
-import com.example.learnit.data.courses.quiz.model.UserResponseData
-import com.example.learnit.data.courses.quiz.repository.QuizResultRepositoryImpl
 import com.example.learnit.databinding.FragmentMultipleChoiceBinding
-import com.example.learnit.ui.feature.courses.quiz.model.AnswerModel
-import com.example.learnit.ui.feature.courses.quiz.model.QuestionsAnswersModel
-import com.example.learnit.ui.feature.courses.quiz.viewModel.MultipleChoiceQuestionAnswerViewModel
+import com.example.learnit.ui.feature.courses.quiz.viewModel.MultipleChoiceQuizViewModel
 import kotlinx.coroutines.launch
-import java.util.Date
 
 class MultipleChoiceQuizFragment : Fragment() {
-    private val viewModel: MultipleChoiceQuestionAnswerViewModel by viewModels()
-
+    private val viewModel: MultipleChoiceQuizViewModel by viewModels()
     private lateinit var binding: FragmentMultipleChoiceBinding
 
     private var courseId: Int = -1
     private var chapterId: Int = -1
     private var lessonId: Int = -1
 
-    private var currentQuizIndex: Int = 0
-    private var quizzes: List<QuestionsAnswersModel<AnswerModel>> = emptyList()
-
     companion object {
-        val TAG: String = MultipleChoiceQuizFragment::class.java.simpleName
+        val TAG: String = FragmentMultipleChoiceBinding::class.java.simpleName
     }
 
     override fun onCreateView(
@@ -45,19 +32,44 @@ class MultipleChoiceQuizFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMultipleChoiceBinding.inflate(inflater, container, false)
+        Log.d(TAG, "TFAdatok: $courseId $chapterId $lessonId")
+        viewModel.loadMultipleChoice(courseId, chapterId, lessonId)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        courseId = arguments?.getInt("courseId", -1) ?: -1
-        chapterId = arguments?.getInt("chapterId", -1) ?: -1
-        lessonId = arguments?.getInt("lessonId", -1) ?: -1
         observeState()
 
-        viewModel.loadMultipleChoice(courseId, chapterId, lessonId)
-        //setUpSubmitButton()
+//        binding.submit.setOnClickListener {
+//            if (viewModel.isResponseSet()) {
+//                viewModel.sendUserResponse(
+//                    QuizResultData(
+//                        uqr_question_id = viewModel.currentQuestion?.questionId!!,
+//                        uqr_user_id = SharedPreferences.getUserId().toInt(),
+//                        response = listOf(
+//                            QuizResponseData(
+//                                option_text = if (viewModel.getUserResponse() == true) "true" else "false",
+//                                is_correct = if (viewModel.getUserResponse() == true) true else false
+//                            )
+//                        ),
+//                        is_correct = 1,
+//                        score = 1,
+//                        response_time = Date()
+//                    )
+//                )
+//                viewModel.resetUserResponse()
+//                setButtonState(binding.trueButton, false)
+//                setButtonState(binding.falseButton, false)
+//            }
+//        }
+
+    }
+
+    fun setQuizData(courseId: Int, chapterId: Int, lessonId: Int) {
+        this.courseId = courseId
+        this.chapterId = chapterId
+        this.lessonId = lessonId
     }
 
     private fun observeState() {
@@ -65,164 +77,246 @@ class MultipleChoiceQuizFragment : Fragment() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.state.collect { state ->
                     when (state) {
-                        is MultipleChoiceQuestionAnswerViewModel.MultipleQuestionPageState.Loading -> {
-                            Log.d(TAG, "Loading multiple choice questions...")
+                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Loading -> {
+                            Log.d(TAG, "Loading questionsAnswers...")
                         }
 
-                        is MultipleChoiceQuestionAnswerViewModel.MultipleQuestionPageState.Success -> {
-                            Log.d(TAG, "Multiple choice questions loaded")
-                            quizzes = state.multipleChoiceData
-                            updateUI(quizzes)
+                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Success -> {
+                            Log.d(TAG, "QuestionsAnswers loaded")
+                            Log.d(TAG, "randomQuestion: ${viewModel.currentQuestion}")
+                            binding.questionTextView.text =
+                                viewModel.currentQuestion?.questionText ?: ""
+                            val answers = viewModel.currentQuestion?.answers
+                            if (answers != null) {
+                                if (answers.isNotEmpty()) {
+                                    val answerTextViews = listOf(
+                                        binding.option1CheckBox,
+                                        binding.option2CheckBox,
+                                        binding.option3CheckBox,
+                                        binding.option4CheckBox,
+                                        binding.option5CheckBox
+                                    )
+                                    for ((index, answer) in answers.withIndex()) {
+                                        answerTextViews[index].text = answer.optionText
+                                    }
+                                }
+                            }
                         }
 
-                        is MultipleChoiceQuestionAnswerViewModel.MultipleQuestionPageState.Failure -> {
-                            Log.e(
-                                TAG,
-                                "Error loading multiple choice questions: ${state.throwable}"
-                            )
+                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Failure -> {
+                            Log.e(TAG, "Error loading QuestionsAnswers: ${state.throwable}")
                         }
                     }
                 }
             }
         }
     }
-
-    private fun updateUI(multipleChoiceData: List<QuestionsAnswersModel<AnswerModel>>) {
-        if (multipleChoiceData.isNotEmpty()) {
-            val currentQuiz = multipleChoiceData[currentQuizIndex]
-            binding.questionTextView.text = currentQuiz.questionText
-
-            val answers = currentQuiz.answers
-
-            if (answers.isNotEmpty()) {
-                val answerTextViews = listOf(
-                    binding.option1CheckBox,
-                    binding.option2CheckBox,
-                    binding.option3CheckBox,
-                    binding.option4CheckBox,
-                    binding.option5CheckBox
-                )
-
-                for ((index, answer) in answers.withIndex()) {
-                    answerTextViews[index].text = answer.optionText
-                }
-            }
-        }
-    }
-
-    private fun loadNextQuiz() {
-        clearCheckBoxes()
-
-        currentQuizIndex++
-        if (currentQuizIndex < quizzes.size) {
-            updateUI(quizzes)
-        } else {
-            Toast.makeText(context, "No more quizzes available.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun handleUserAnswers() {
-        val selectedAnswers = getSelectedAnswers()
-        val questionsAnswersData = createQuizResponseData(selectedAnswers)
-        Log.d(TAG, "handleUserAnswers: $questionsAnswersData")
-
-        lifecycleScope.launch {
-            try {
-                val response =
-                    QuizResultRepositoryImpl.sendResponse(questionsAnswersData)
-                if (response.success) {
-                    val totalQuestions = questionsAnswersData.response.size
-                    //val grade = (correctResponses.toDouble() / totalQuestions * 100).toInt()
-
-//                    Toast.makeText(
-//                        context,
-//                        "User response recorded successfully. Grade: $grade%",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    updateScore(grade)
-                } else {
-                    Toast.makeText(context, "Failed to record user response.", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            } catch (e: Exception) {
-                Log.e(TAG, "Error sending quiz response: ${e.message}")
-                Toast.makeText(context, "Error sending quiz response.", Toast.LENGTH_SHORT).show()
-            } finally {
-                loadNextQuiz()
-            }
-        }
-    }
-
-    private fun createQuizResponseData(selectedAnswers: List<AnswerData>): UserResponseData {
-        val currentQuiz = quizzes[currentQuizIndex]
-        return UserResponseData(
-            uqr_question_id = currentQuiz.questionId,
-            uqr_user_id = 4,
-            response = listOf(
-                UserAnswerData(
-                    optionText = if (viewModel.getUserResponse().optionText.isEmpty()) {
-                        selectedAnswers[0].option_text
-                    } else {
-                        viewModel.getUserResponse().optionText
-                    },
-                    isCorrect = viewModel.getUserResponse().isCorrect
-                )
-            ),
-            is_correct = 1,
-            score = 1,
-            response_time = Date()
-        )
-    }
-
-//    private fun setUpSubmitButton() {
-//        binding.submitButton.setOnClickListener {
-//            handleUserAnswers()
+}
+//    private val viewModel: MultipleChoiceQuestionAnswerViewModel by viewModels()
+//
+//    private lateinit var binding: FragmentMultipleChoiceBinding
+//
+//    private var courseId: Int = -1
+//    private var chapterId: Int = -1
+//    private var lessonId: Int = -1
+//
+//    private var currentQuizIndex: Int = 0
+//    private var quizzes: List<QuestionsAnswersModel<AnswerModel>> = emptyList()
+//
+//    companion object {
+//        val TAG: String = MultipleChoiceQuizFragment::class.java.simpleName
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentMultipleChoiceBinding.inflate(inflater, container, false)
+//        Log.d(TrueFalseQuizFragment.TAG, "MCAdatok: $courseId $chapterId $lessonId")
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        observeState()
+//        viewModel.loadMultipleChoice(courseId, chapterId, lessonId)
+//        //setUpSubmitButton()
+//    }
+//
+//    private fun observeState() {
+//        lifecycleScope.launch {
+//            repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                viewModel.state.collect { state ->
+//                    when (state) {
+//                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Loading -> {
+//                            Log.d(TAG, "Loading questionsAnswers...")
+//                        }
+//
+//                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Success -> {
+//                            Log.d(TrueFalseQuizFragment.TAG, "QuestionsAnswers loaded")
+//                            Log.d(
+//                                TrueFalseQuizFragment.TAG,
+//                                "randomQuestion: ${viewModel.currentQuestion}"
+//                            )
+//                            binding.lessonNumber.text =
+//                                viewModel.currentQuestion?.questionId.toString()
+//                            binding.question.text = viewModel.currentQuestion?.questionText
+//                        }
+//
+//                        is MultipleChoiceQuizViewModel.MultipleQuestionPageState.Failure -> {
+//                            Log.e(
+//                                TrueFalseQuizFragment.TAG,
+//                                "Error loading QuestionsAnswers: ${state.throwable}"
+//                            )
+//                        }
+//                    }
+//                }
+//            }
 //        }
 //    }
-
-    private fun getSelectedAnswers(): List<AnswerData> {
-        val selectedAnswers = mutableListOf<AnswerData>()
-
-        val answerTextViews = listOf(
-            binding.option1CheckBox,
-            binding.option2CheckBox,
-            binding.option3CheckBox,
-            binding.option4CheckBox,
-            binding.option5CheckBox
-        )
-
-        for ((index, textView) in answerTextViews.withIndex()) {
-            if (textView.isChecked) {
-                val answerText = textView.text.toString()
-                val isCorrect = quizzes[currentQuizIndex].answers[index].isCorrect
-                selectedAnswers.add(AnswerModel(answerText, isCorrect).mapToAnswerData())
-            }
-        }
-
-        return selectedAnswers
-    }
-
-    private fun AnswerData.mapToAnswerModel(): AnswerModel {
-        return AnswerModel(optionText = option_text, isCorrect = is_correct)
-    }
-
-
-    private fun updateScore(newScore: Int) {
-        Log.d(TAG, "Score updated: $newScore")
-    }
-
-    private fun clearCheckBoxes() {
-        val answerCheckBoxes = listOf(
-            binding.option1CheckBox,
-            binding.option2CheckBox,
-            binding.option3CheckBox,
-            binding.option4CheckBox,
-            binding.option5CheckBox
-        )
-
-        for (checkBox in answerCheckBoxes) {
-            checkBox.isChecked = false
-        }
-    }
-
-}
+//
+//    fun setQuizData(courseId: Int, chapterId: Int, lessonId: Int) {
+//        this.courseId = courseId
+//        this.chapterId = chapterId
+//        this.lessonId = lessonId
+//    }
+//
+//    private fun updateUI(multipleChoiceData: List<QuestionsAnswersModel<AnswerModel>>) {
+//        if (multipleChoiceData.isNotEmpty()) {
+//            val currentQuiz = multipleChoiceData[currentQuizIndex]
+//            binding.questionTextView.text = currentQuiz.questionText
+//
+//            val answers = currentQuiz.answers
+//
+//            if (answers.isNotEmpty()) {
+//                val answerTextViews = listOf(
+//                    binding.option1CheckBox,
+//                    binding.option2CheckBox,
+//                    binding.option3CheckBox,
+//                    binding.option4CheckBox,
+//                    binding.option5CheckBox
+//                )
+//
+//                for ((index, answer) in answers.withIndex()) {
+//                    answerTextViews[index].text = answer.optionText
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun loadNextQuiz() {
+//        clearCheckBoxes()
+//
+//        currentQuizIndex++
+//        if (currentQuizIndex < quizzes.size) {
+//            updateUI(quizzes)
+//        } else {
+//            Toast.makeText(context, "No more quizzes available.", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+//
+//    private fun handleUserAnswers() {
+//        val selectedAnswers = getSelectedAnswers()
+//        val questionsAnswersData = createQuizResponseData(selectedAnswers)
+//        Log.d(TAG, "handleUserAnswers: $questionsAnswersData")
+//
+//        lifecycleScope.launch {
+//            try {
+//                val response =
+//                    QuizResultRepositoryImpl.sendResponse(questionsAnswersData)
+//                if (response.success) {
+//                    val totalQuestions = questionsAnswersData.response.size
+//                    //val grade = (correctResponses.toDouble() / totalQuestions * 100).toInt()
+//
+////                    Toast.makeText(
+////                        context,
+////                        "User response recorded successfully. Grade: $grade%",
+////                        Toast.LENGTH_SHORT
+////                    ).show()
+////                    updateScore(grade)
+//                } else {
+//                    Toast.makeText(context, "Failed to record user response.", Toast.LENGTH_SHORT)
+//                        .show()
+//                }
+//            } catch (e: Exception) {
+//                Log.e(TAG, "Error sending quiz response: ${e.message}")
+//                Toast.makeText(context, "Error sending quiz response.", Toast.LENGTH_SHORT).show()
+//            } finally {
+//                loadNextQuiz()
+//            }
+//        }
+//    }
+//
+//    private fun createQuizResponseData(selectedAnswers: List<AnswerData>): UserResponseData {
+//        val currentQuiz = quizzes[currentQuizIndex]
+//        return UserResponseData(
+//            uqr_question_id = currentQuiz.questionId,
+//            uqr_user_id = 4,
+//            response = listOf(
+//                UserAnswerData(
+//                    optionText = if (viewModel.getUserResponse().optionText.isEmpty()) {
+//                        selectedAnswers[0].option_text
+//                    } else {
+//                        viewModel.getUserResponse().optionText
+//                    },
+//                    isCorrect = viewModel.getUserResponse().isCorrect
+//                )
+//            ),
+//            is_correct = 1,
+//            score = 1,
+//            response_time = Date()
+//        )
+//    }
+//
+////    private fun setUpSubmitButton() {
+////        binding.submitButton.setOnClickListener {
+////            handleUserAnswers()
+////        }
+////    }
+//
+//    private fun getSelectedAnswers(): List<AnswerData> {
+//        val selectedAnswers = mutableListOf<AnswerData>()
+//
+//        val answerTextViews = listOf(
+//            binding.option1CheckBox,
+//            binding.option2CheckBox,
+//            binding.option3CheckBox,
+//            binding.option4CheckBox,
+//            binding.option5CheckBox
+//        )
+//
+//        for ((index, textView) in answerTextViews.withIndex()) {
+//            if (textView.isChecked) {
+//                val answerText = textView.text.toString()
+//                val isCorrect = quizzes[currentQuizIndex].answers[index].isCorrect
+//                selectedAnswers.add(AnswerModel(answerText, isCorrect).mapToAnswerData())
+//            }
+//        }
+//
+//        return selectedAnswers
+//    }
+//
+//    private fun AnswerData.mapToAnswerModel(): AnswerModel {
+//        return AnswerModel(optionText = option_text, isCorrect = is_correct)
+//    }
+//
+//
+//    private fun updateScore(newScore: Int) {
+//        Log.d(TAG, "Score updated: $newScore")
+//    }
+//
+//    private fun clearCheckBoxes() {
+//        val answerCheckBoxes = listOf(
+//            binding.option1CheckBox,
+//            binding.option2CheckBox,
+//            binding.option3CheckBox,
+//            binding.option4CheckBox,
+//            binding.option5CheckBox
+//        )
+//
+//        for (checkBox in answerCheckBoxes) {
+//            checkBox.isChecked = false
+//        }
+//    }
+//}
--- app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/QuizFragment.kt
+++ app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/QuizFragment.kt
@@ -1,6 +1,7 @@
 package com.example.learnit.ui.feature.courses.quiz.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +10,17 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.learnit.R
 import com.example.learnit.databinding.FragmentQuizBinding
+import com.example.learnit.ui.feature.cou.MultipleChoiceQuizFragment
 
 class QuizFragment : Fragment() {
 
     private lateinit var binding: FragmentQuizBinding
+    private var currentFragmentIndex = 0
+    private val quizTypes = listOf("multiple_choice", "true_false")
+    private val random = java.util.Random()
+    private var courseId: Int = -1
+    private var chapterId: Int = -1
+    private var lessonId: Int = -1
 
     companion object {
         val TAG: String = QuizFragment::class.java.simpleName
@@ -24,6 +32,12 @@ class QuizFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentQuizBinding.inflate(inflater, container, false)
+
+        courseId = arguments?.getInt("courseId", -1) ?: -1
+        chapterId = arguments?.getInt("chapterId", -1) ?: -1
+        lessonId = arguments?.getInt("lessonId", -1) ?: -1
+
+        Log.d(TAG, "adatok: $courseId $chapterId $lessonId")
         return binding.root
     }
 
@@ -33,19 +47,42 @@ class QuizFragment : Fragment() {
         binding.escapeButton.setOnClickListener {
             showExitConfirmationDialog()
         }
-
         binding.bookButton.setOnClickListener {
             findNavController().navigate(R.id.action_quizFragment_to_TheoryFragment)
+        }
+        binding.checkAndSubmitButton.setOnClickListener {
+            onNextButtonClicked()
         }
     }
 
     private fun showQuizFragment() {
-        val trueFalseFragment = TrueFalseQuizFragment()
+        val fragment = when (val randomQuizType = quizTypes[random.nextInt(quizTypes.size)]) {
+            "multiple_choice"  -> {
+                val multipleChoiceFragment = MultipleChoiceQuizFragment()
+                multipleChoiceFragment.setQuizData(courseId, chapterId, lessonId)
+                multipleChoiceFragment
+            }
+            "true_false" -> {
+                val trueFalseFragment = TrueFalseQuizFragment()
+                trueFalseFragment.setQuizData(courseId, chapterId, lessonId)
+                trueFalseFragment
+            }
+            else -> throw IllegalArgumentException("Invalid quiz type: $randomQuizType")
+        }
+
         childFragmentManager.beginTransaction()
-            .replace(R.id.frameQuizContainer, trueFalseFragment)
+            .replace(R.id.frameQuizContainer, fragment)
             .commit()
     }
 
+    private fun onNextButtonClicked() {
+        currentFragmentIndex++
+        if (currentFragmentIndex < 10) {
+            showQuizFragment()
+        } else {
+            Log.d(TAG, "Quiz finished")
+        }
+    }
 
     private fun showExitConfirmationDialog() {
         AlertDialog.Builder(requireContext())
