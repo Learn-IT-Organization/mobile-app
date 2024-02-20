bbbaf0c976f1d589f15c88942a9cb6c0d433a616
DemeterEvelin14 <99736246+DemeterEvelin14@users.noreply.github.com>
2024-02-20 22:14:32 +0200

    #123: Multiple choice screen design changed

--- app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/MultipleChoiceQuizFragment.kt
+++ app/src/main/java/com/example/learnit/ui/feature/courses/quiz/fragment/MultipleChoiceQuizFragment.kt
@@ -1,10 +1,10 @@
-package com.example.learnit.ui.feature.courses.quiz.fragment
-
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.learnit.R
 import com.example.learnit.data.SharedPreferences
@@ -12,14 +12,19 @@ import com.example.learnit.data.courses.quiz.model.MultipleChoiceQuestionData
 import com.example.learnit.data.courses.quiz.model.QuizResponseData
 import com.example.learnit.databinding.FragmentQuizMultipleChoiceBinding
 import com.example.learnit.ui.feature.courses.quiz.QuizButtonClickListener
+import com.example.learnit.ui.feature.courses.quiz.fragment.BaseQuizFragment
+import com.example.learnit.ui.feature.courses.quiz.fragment.QuizFragment
 import com.example.learnit.ui.feature.courses.quiz.viewModel.SharedQuizViewModel
 import java.util.Date
 
 class MultipleChoiceQuizFragment : BaseQuizFragment<MultipleChoiceQuestionData>(),
     QuizButtonClickListener {
+
     override lateinit var binding: FragmentQuizMultipleChoiceBinding
     override val viewModel: SharedQuizViewModel by activityViewModels()
     override val TAG: String = MultipleChoiceQuizFragment::class.java.simpleName
+
+    private val selectedButtons = mutableListOf<Button>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,22 +40,64 @@ class MultipleChoiceQuizFragment : BaseQuizFragment<MultipleChoiceQuestionData>(
 
         binding.verifyButton.isEnabled = false
 
-        binding.verifyButton.setOnClickListener {
-            onNextButtonClicked()
-        }
-
-    }
-
-    private fun clearCheckBoxes() {
-        val answerCheckBoxes = listOf(
-            binding.option1CheckBox,
-            binding.option2CheckBox,
-            binding.option3CheckBox,
-            binding.option4CheckBox,
+        val optionButtons = listOf(
+            binding.option1Button,
+            binding.option2Button,
+            binding.option3Button,
+            binding.option4Button
         )
 
-        for (checkBox in answerCheckBoxes) {
-            checkBox.isChecked = false
+        optionButtons.forEach { button ->
+            button.setOnClickListener {
+                button.isSelected = !button.isSelected
+                updateButtonAppearance(button)
+            }
+        }
+
+        binding.verifyButton.setOnClickListener {
+            if (hasAtLeastOneAnswerSelected()) {
+                onNextButtonClicked()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.please_select_an_answer),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun updateButtonAppearance(button: Button) {
+        if (button.isSelected) {
+            button.setBackgroundColor(resources.getColor(R.color.md_theme_inversePrimary_mediumContrast))
+            binding.verifyButton.isEnabled = true
+            updateVerifyButtonStyle(true)
+            selectedButtons.add(button)
+        } else {
+            button.setBackgroundColor(resources.getColor(R.color.md_theme_secondary_mediumContrast))
+            selectedButtons.remove(button)
+            if (selectedButtons.isEmpty()) {
+                binding.verifyButton.isEnabled = false
+                updateVerifyButtonStyle(false)
+            }
+        }
+    }
+
+    private fun hasAtLeastOneAnswerSelected(): Boolean {
+        return selectedButtons.isNotEmpty()
+    }
+
+    private fun clearButtons() {
+        val optionButtons = listOf(
+            binding.option1Button,
+            binding.option2Button,
+            binding.option3Button,
+            binding.option4Button
+        )
+
+        optionButtons.forEach {
+            it.isSelected = false
+            updateButtonAppearance(it)
         }
     }
 
@@ -64,7 +111,7 @@ class MultipleChoiceQuizFragment : BaseQuizFragment<MultipleChoiceQuestionData>(
                 score = 0.0f
             )
         )
-        clearCheckBoxes()
+        clearButtons()
         QuizFragment.viewPager.currentItem += 1
         Log.d(TAG, "currentItem: ${QuizFragment.viewPager.currentItem}")
         QuizFragment.currentQuestionNumber.postValue(
@@ -76,42 +123,48 @@ class MultipleChoiceQuizFragment : BaseQuizFragment<MultipleChoiceQuestionData>(
     }
 
     private fun getSelectedAnswers(): List<Boolean> {
-        val selectedAnswers = mutableListOf<Boolean>()
-
-        val answerCheckBoxes = listOf(
-            binding.option1CheckBox,
-            binding.option2CheckBox,
-            binding.option3CheckBox,
-            binding.option4CheckBox,
+        val optionButtons = listOf(
+            binding.option1Button,
+            binding.option2Button,
+            binding.option3Button,
+            binding.option4Button
         )
 
-        for (checkBox in answerCheckBoxes) {
-            selectedAnswers.add(checkBox.isChecked)
-        }
+        val selectedIndices = optionButtons.indices.filter { optionButtons[it].isSelected }
 
-        return selectedAnswers
+        val sortedSelectedIndices = selectedIndices.sorted()
+
+        return List(4) { index -> sortedSelectedIndices.contains(index) }
+    }
+
+    private fun updateVerifyButtonStyle(isEnabled: Boolean) {
+        if (isEnabled) {
+            binding.verifyButton.setBackgroundColor(resources.getColor(R.color.md_theme_secondary_mediumContrast))
+            binding.verifyButton.setTextColor(resources.getColor(R.color.md_theme_onPrimary))
+        } else {
+           binding.verifyButton.setBackgroundColor(resources.getColor(R.color.md_theme_outlineVariant_mediumContrast))
+            binding.verifyButton.setTextColor(resources.getColor(R.color.md_theme_outline_mediumContrast))
+        }
     }
 
     override fun updateUI() {
         binding.questionTextView.text = currentQuestion?.questionText
         val answers = currentQuestion?.answers
+
         if (answers != null) {
             if (answers.isNotEmpty()) {
-                val answerTextViews = listOf(
-                    binding.option1CheckBox,
-                    binding.option2CheckBox,
-                    binding.option3CheckBox,
-                    binding.option4CheckBox,
+                val optionButtons = listOf(
+                    binding.option1Button,
+                    binding.option2Button,
+                    binding.option3Button,
+                    binding.option4Button
                 )
+
                 for ((index, answer) in answers.withIndex()) {
-                    answerTextViews[index].text = answer
+                    optionButtons[index].text = answer
+                    updateButtonAppearance(optionButtons[index])
                 }
             }
         }
-    }
-
-    private fun updateButtonStyle() {
-        binding.verifyButton.setBackgroundColor(resources.getColor(R.color.md_theme_secondary_mediumContrast))
-        binding.verifyButton.setTextColor(resources.getColor(R.color.md_theme_onPrimary))
     }
 }
